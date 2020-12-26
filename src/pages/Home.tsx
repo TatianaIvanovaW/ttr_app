a7e40c323b9c824ef907c13ade5b18a207206689
Tatiana Ivanova <ivanova.tatiana.w@gmail.com>
2020-12-26 23:12:55 +0100

    setup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,32 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  // IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import React from "react";
+// import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle
+            style={{
+              fontFamily: "'Cinzel Decorative', cursive",
+              fontSize: "30px",
+            }}
+          >
+            Ticket To Ride
+          </IonTitle>
+          <IonTitle style={{ fontFamily: "'Nova Cut', cursive" }}>
+            Europe
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
     </IonPage>
   );
 };
